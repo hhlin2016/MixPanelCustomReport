e96341ba0a5c147d43d48565c35c8d304f773384
hhlin2016 <lin.hsiaohong@gmail.com>
2016-04-14 11:38:44 -0400

    Forgot a comment

--- files_to_review/customQueryGenerator.js
+++ files_to_review/customQueryGenerator.js
@@ -76,6 +76,7 @@ function buildCustomQueryFunctionScript(propertySelector, selector){
 				event_selectors: [{event: params.event}]})';
 	// filter by event
 	var eventFilter = '.filter(function(event) { return (event.name === params.event ) })';
+	// Remove null and undefined entries in Platform
 	var generalPlatformFilter = '.filter(function(event) { return (event.properties.Platform)})';
 	// filter by properties (Platform, App Name, and $model)
 	var propertyFilters = '';
